c06a09719cd3fa447d606e736dd6afeea0ca040c
Swanand Pagnis <swanand.pagnis@gmail.com>
2020-06-09 22:21:24 +0530

    Add a simple, unobtrusive subscription form.

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+// Read all the options available here:
+// https://prettier.io/docs/en/options.html
+// If you want to ignore certain statement or block, add this comment at the end of line: // prettier-ignore
+module.exports = {
+  printWidth: 120,
+  singleQuote: false,
+};
